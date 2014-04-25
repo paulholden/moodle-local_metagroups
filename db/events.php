58cd87643715c4eb2cf2ef2e2625c159c4b55a9d
François Lumineau <flumineau@iae.univ-poitiers.fr>
2014-04-25 10:20:17 +0200

    Add groupings synchronization

--- db/events.php
+++ db/events.php
@@ -24,6 +24,21 @@ defined('MOODLE_INTERNAL') || die();
 
 $observers = array(
     array(
+        'eventname' => '\core\event\grouping_created',
+        'callback'  => '\local_metagroups\observers::grouping_created',
+    ),
+
+    array(
+        'eventname' => '\core\event\grouping_updated',
+        'callback'  => '\local_metagroups\observers::grouping_updated',
+    ),
+
+    array(
+        'eventname' => '\core\event\grouping_deleted',
+        'callback'  => '\local_metagroups\observers::grouping_deleted',
+    ),
+	
+    array(
         'eventname' => '\core\event\group_created',
         'callback'  => '\local_metagroups\observers::group_created',
     ),
