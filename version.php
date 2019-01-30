ce3a5d16c61a547d1523a9d6c732899a9b19e15b
Paul Holden <pholden@greenhead.ac.uk>
2019-01-30 16:21:25 +0000

    Bump version for new release.

--- version.php
+++ version.php
@@ -23,8 +23,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_metagroups';
-$plugin->release   = '2.0';
-$plugin->version   = 2018120700;
+$plugin->release   = '2.1';
+$plugin->version   = 2018120701;
 $plugin->requires  = 2017051500; // Moodle 3.3 onwards.
 $plugin->maturity  = MATURITY_STABLE;
 
