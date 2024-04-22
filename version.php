f62fef53d194f1f32366bbeba0125c7451a2edff
Paul Holden <paulh@moodle.com>
2024-04-22 21:28:02 +0100

    Moodle 4.4 release.

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_metagroups';
-$plugin->release   = '4.3';
-$plugin->version   = 2023121100;
+$plugin->release   = '4.4';
+$plugin->version   = 2024042200;
 $plugin->requires  = 2022112800; // Moodle 4.1 onwards.
 $plugin->maturity  = MATURITY_STABLE;
 
